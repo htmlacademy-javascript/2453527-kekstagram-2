d004e123651f9f7d1199af2d1bd82049060cb9b7
Keks <keks@htmlacademy.ru>
2024-11-09 15:57:44 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/create-picture-list.js
@@ -0,0 +1,26 @@
+import {findTemplate, renderPack} from './utils.js';
+import {mocksCards} from './mocks.js';
+
+
+const pictureList = document.querySelector('.pictures');
+const pictureTemplate = findTemplate('picture');
+
+const pictureListFragment = document.createDocumentFragment();
+
+// Функция, создающая картинку
+const getPictureElement = ({url, description, likes, comments}) => {
+  const pictureElement = pictureTemplate.cloneNode(true);
+  pictureElement.querySelector('.picture__img')
+    .setAttribute('src', url);
+  pictureElement.querySelector('.picture__img')
+    .setAttribute('alt', description);
+  pictureElement.querySelector('.picture__comments')
+    .textContent = comments.length;
+  pictureElement.querySelector('.picture__likes')
+    .textContent = likes;
+  pictureListFragment.appendChild(pictureElement);
+
+  return pictureElement;
+};
+
+renderPack(mocksCards, getPictureElement, pictureList);
---
+++ 8/js/main.js
@@ -0,0 +1,4 @@
+import './create-picture-list.js';
+import './render-full-post.js';
+import './form-validation.js';
+import './filter-editor.js';
